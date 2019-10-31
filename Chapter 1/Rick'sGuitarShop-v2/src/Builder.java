f4c3bd45632c5bf3cdeba284380ea47fddcaeade
Mushfiqul Huda <mushfiqul.onee@gmail.com>
2019-10-31 17:37:32 +0100

    Strings have been replaced by enums

---
+++ Chapter 1/Rick'sGuitarShop-v2/src/Builder.java
@@ -0,0 +1,24 @@
+
+public enum Builder {
+	
+	FENDER, MARTIN, GIBSON, COLLINGS, TAYLOR;
+	
+	public String toString() {
+		switch(this) {
+			case FENDER:
+				return "fender";
+			case MARTIN:
+				return "martin";
+			case GIBSON:
+				return "gibson";
+			case COLLINGS:
+				return "collings";
+			case TAYLOR:
+				return "taylor";
+			 default:
+				return "unspecified";
+		}
+	}
+	
+
+}
---
+++ Chapter 1/Rick'sGuitarShop-v2/src/FindGuitarTester.java
@@ -0,0 +1,28 @@
+
+public class FindGuitarTester {
+	
+	public static void main(String[] args) {
+		Inventory inventory = new Inventory();
+		initializeInventory(inventory);
+		
+		Guitar desired = new Guitar("", 0, Builder.FENDER, "Stratocastor", 
+				Type.ELECTRIC, Wood.ALDER, Wood.ALDER);
+		Guitar guitar = inventory.search(desired);
+		if(guitar != null) {
+			System.out.println("Erin, you might like this: " + 
+						guitar.getBuilder() + " " + guitar.getModel() + " " +
+						guitar.getType() + " guitar :\n " +
+						guitar.getBackWood() + " back " + 
+						guitar.getTopWood() + " top. \nYou can have it for only " +
+						guitar.getPrice() + "Euros!");
+		}else {
+			System.out.println("Sorry, Erin, we have nothing for you.");
+		}
+	}
+	
+	private static void initializeInventory(Inventory inventory) {
+		
+		inventory.addGuitar("V95693", 1500, Builder.FENDER, "Stratocastor", Type.ELECTRIC, Wood.ALDER, Wood.ALDER);
+		inventory.addGuitar("F95623", 1400, Builder.FENDER, "Stratocastor", Type.ELECTRIC, Wood.ALDER, Wood.ALDER);
+	}
+}
